1f25f2ccea65df0c4a05337df45bab9a40f32169
KoheiH992 <testhashi82@gmail.com>
2025-06-25 14:12:28 +0900

    Add files via upload

---
+++ FLASK_APP/add_testuser.py
@@ -0,0 +1,10 @@
+from company_blog import db
+from company_blog.models import User
+
+
+user_list = []
+for i in range(60):
+    user_list.append(User(f"temp_user{i}@test.com", f"Temp User{i}", "111", "0"))
+
+db.session.add_all(user_list)
+db.session.commit()
